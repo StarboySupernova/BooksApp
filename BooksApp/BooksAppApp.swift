97e74bda126ed9e856d82d5dfeaf28adc9cf9f6f
Christian Simbarashe <supernovaonline@outlook.com>
2021-10-25 02:17:57 +0100

    Initial Commit

---
+++ BooksApp/BooksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksAppApp.swift
+//  BooksApp
+//
+//  Created by user207077 on 10/25/21.
+//
+
+import SwiftUI
+
+@main
+struct BooksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
